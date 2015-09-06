8f281a392f3f80183c728ee060904adda55eb466
syobochim <syobochim@gmail.com>
2015-09-06 16:42:43 +0900

    Daoクラス作った

---
+++ src/main/java/com/kajipoi/dao/HouseDao.java
@@ -0,0 +1,27 @@
+package com.kajipoi.dao;
+
+import com.kajipoi.domain.Id;
+import com.kajipoi.domain.Name;
+import com.kajipoi.entity.House;
+import org.seasar.doma.*;
+
+import java.util.List;
+
+@Dao
+public interface HouseDao {
+
+    @Select(strategy = SelectType.STREAM)
+    List<House> findAll();
+
+    @Select(ensureResult = true)
+    House findById(Id houseId);
+
+    @Select(ensureResult = true)
+    House findByHouseName(Name houseName);
+
+    @Insert
+    int save(House house);
+
+    @Delete
+    int delete(House house);
+}
