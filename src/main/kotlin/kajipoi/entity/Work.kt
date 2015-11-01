8b5493a222e9d3a8886eb6eb54dd297824bb0382
syobochim <syobochim@gmail.com>
2015-11-02 00:48:41 +0900

    家事した日をもう一回追加

--- src/main/kotlin/kajipoi/entity/Work.kt
+++ src/main/kotlin/kajipoi/entity/Work.kt
@@ -2,13 +2,16 @@ package kajipoi.entity
 
 import kajipoi.domain.Id
 import kajipoi.domain.Status
+import kajipoi.domain.WorkDate
 import org.seasar.doma.Entity
 import org.seasar.doma.jdbc.entity.NamingType
 
 /**
+ * メンバーがやったお仕事の記録用テーブル
  * @author syobochim
  */
 @Entity(naming = NamingType.LOWER_CASE, immutable = true)
 class Work(val memberId: Id,
            val houseworkId: Id,
-           val status: Status)
+           val status: Status,
+           val workDate: WorkDate)
