68d25c3631f9ae4e5a8ee9f8f6cf7d9c36c90f47
syobochim <syobochim@gmail.com>
2015-12-29 22:02:58 +0900

    domainはkotlinに寄せる。日付系の型が。。。

--- src/main/kotlin/com/syobochim/kajipoi/domain/domain.kt
+++ src/main/kotlin/com/syobochim/kajipoi/domain/domain.kt
@@ -5,29 +5,26 @@ import java.util.*
 
 /**
  * Idを表すドメイン
- *
- * @author syobochim
- * @since 1.0
  */
 @Domain(valueType = UUID::class)
 class Id(val value: UUID)
 
 /**
  * 名前をあわらすドメインクラス。
- *
- * @author syobochim
- * @since 1.0
  */
 @Domain(valueType = String::class)
 class Name(val value: String)
 
 /**
  * ステータスを表すドメインクラス。
- *
- * @author syobochim
- * @since 1.0
  */
 @Domain(valueType = Int::class)
 enum class Status(val value: Int) {
     DOING(0), NOTHING(1), DONE(2)
 }
+
+/**
+ * 家事実施日を表すドメインクラス。
+ */
+@Domain(valueType = Date::class)
+class WorkDate(val value: Date)
