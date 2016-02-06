8d8de529be02bfa4facb9419c2d9f6bd058c121f
syobochim <syobochim@gmail.com>
2016-02-06 23:42:38 +0900

    refs #1 Keyドメインのコンストラクタにチェックを追加。

--- src/main/java/com/syobochim/kajipoi/domain/Key.java
+++ src/main/java/com/syobochim/kajipoi/domain/Key.java
@@ -2,6 +2,8 @@ package com.syobochim.kajipoi.domain;
 
 import org.seasar.doma.Domain;
 
+import java.util.Objects;
+
 /**
  * Idを表すドメイン
  *
@@ -13,14 +15,20 @@ public class Key<ENTITY> {
     private final Long value;
 
     public Key(Long value) {
+        Objects.requireNonNull(value, "key must not be null.");
         this.value = value;
-    }
-
-    public Key<ENTITY> setValue(Long value) {
-        return new Key<>(value);
     }
 
     public Long getValue() {
         return value;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    public static <ENTITY> Key<ENTITY> valueOf(String value) {
+        return new Key<>(Long.valueOf(value));
     }
 }
