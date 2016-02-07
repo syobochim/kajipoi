be00fd024b6e0fa0e515fdd54d8aee49f884572d
syobochim <syobochim@gmail.com>
2016-02-07 19:05:02 +0900

    close #1 LocaleDate.parse！！でvalueOfしたー。とりあえず、全部のドメインにrequireNonNull！！

--- src/main/java/com/syobochim/kajipoi/domain/WorkDate.java
+++ src/main/java/com/syobochim/kajipoi/domain/WorkDate.java
@@ -3,6 +3,7 @@ package com.syobochim.kajipoi.domain;
 import org.seasar.doma.Domain;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 /**
  * 家事実施日を表すドメインクラス。
@@ -15,10 +16,20 @@ public class WorkDate {
     private final LocalDate value;
 
     public WorkDate(LocalDate value) {
+        Objects.requireNonNull(value, "workDate must not be null");
         this.value = value;
     }
 
     public LocalDate getValue() {
         return value;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    public static WorkDate valueOf(String text) {
+        return new WorkDate(LocalDate.parse(text));
     }
 }
