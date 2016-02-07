d5c26f7f61734e20a24a74a8d401d375d46d07c9
syobochim <syobochim@gmail.com>
2016-02-07 18:58:49 +0900

    refs #1 valueOfはnullを許可するけど、ドメイン（型）自体にはnullを許可させない。だから、valueOfで返るのはnullであって、valueにnullがつまったNameではない！

--- src/main/java/com/syobochim/kajipoi/domain/Name.java
+++ src/main/java/com/syobochim/kajipoi/domain/Name.java
@@ -2,6 +2,9 @@ package com.syobochim.kajipoi.domain;
 
 import org.seasar.doma.Domain;
 
+import java.util.Objects;
+import java.util.Optional;
+
 /**
  * 名前をあわらすドメインクラス。
  *
@@ -12,10 +15,24 @@ public class Name {
     private final String value;
 
     public Name(String value) {
+        Objects.requireNonNull(value, "name must not be null");
+        if (value.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         this.value = value;
     }
 
     public String getValue() {
         return value;
     }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public static Name valueOf(String value) {
+        return Optional.ofNullable(value).filter(a -> !a.isEmpty()).map(Name::new).orElse(null);
+    }
+
 }
